703f8e7f527113defb7afbff14ec08a0e8754681
unknown <clirimajeti@gmail.com>
2019-02-26 15:07:54 +0100

    routing

--- js/app.js
+++ js/app.js
@@ -1,23 +1,19 @@
 var app = angular.module('app', ['ngRoute','ui.bootstrap','ngTouch','ngAnimate']);
 
-app.config(function ($locationProvider) {
-  $locationProvider.html5Mode(true);
-});
-
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {
-      templateUrl: '/views/page1.html',
-      controller: 'Ctrl'
-    });
-    $routeProvider.when('/1', {
-      templateUrl: '/views/page2.html',
-      controller: 'Ctrl'
-    });
-    $routeProvider.when('/2', {
-      templateUrl: '/views/page3.html',
-      controller: 'Ctrl'
-    });
-    $routeProvider.otherwise({redirectTo:'/'});
+  $routeProvider.when('/', {
+    templateUrl: 'https://clirimajeti.github.io/SP-Template/views/page1.html',
+    controller: 'Ctrl'
+  });
+  $routeProvider.when('/1', {
+    templateUrl: 'https://clirimajeti.github.io/SP-Template/views/page2.html',
+    controller: 'Ctrl'
+  });
+  $routeProvider.when('/2', {
+    templateUrl: 'https://clirimajeti.github.io/SP-Template/views/page3.html',
+    controller: 'Ctrl'
+  });
+  $routeProvider.otherwise({redirectTo:'/'});
 }]);
 
 app.controller("Ctrl", ["$scope", function ($scope) {
